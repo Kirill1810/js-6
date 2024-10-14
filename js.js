de31dc7c52eff02237f1c8a424a2aa970bf50ac6
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-10-14 18:36:33 +0300

    kkk

---
+++ js.js
@@ -0,0 +1,59 @@
+//1
+
+// let form =  1;
+// const form1 =  11;
+
+// while (form < form1){
+
+//     console.log(form)
+//     form += 1;
+// }
+
+
+//2
+
+// for (let i = 2; i < 20; i = i + 2) { console.log(i) }
+
+
+//3
+
+// for (let i = 7; i < 70; i = i + 7) { console.log(i) }
+
+
+//4
+
+// const arr = [1, 2, 3 , 4 , 5];
+
+
+
+// while (arr){
+
+//         console.log(arr)
+//          arr += 1;
+//     }
+
+//5
+
+//   const  numbers = [1, 2, 3 , 4 , 5 ,6 ,8 ,9 ,10];
+
+
+
+//   for (let i = 1; i <= numbers.length; i ++) {
+//     if (i === 7) {
+//       break;
+//     }
+//     console.log(i);
+//   }
+
+//6
+
+const n = 15;
+
+for (let i = 1; i < 18; i++) {
+
+  if (i >= n) {
+    break;
+  }
+  
+  console.log(i);
+}
